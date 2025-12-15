ad5fd808bc2a85ef4e30946a69577d6190025e61
Andrea <andrea@zampierolo.dev>
2025-12-15 14:58:49 +0100

    fix: configures image domains for Next.js
    Adds remotePatterns configuration to next.config.ts,
    allowing images from specified domains to be optimized and
    served by Next.js Image Optimization API.
    
    This enhances performance and security by explicitly allowing
    image optimization for the configured external image providers.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'image.tmdb.org',
+			},
+			{
+				protocol: 'https',
+				hostname: 'www.gravatar.com',
+			},
+			{
+				protocol: 'https',
+				hostname: 'secure.gravatar.com',
+			},
+		],
+	},
 };
 
 export default nextConfig;
